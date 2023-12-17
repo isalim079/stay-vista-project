6a16d52ecc0a3beb17c6c8283733938d2cfc5014
isalim079 <salimreza1025@gmail.com>
2023-12-17 19:01:31 +0600

    Heading added in  Rooms section

--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -2,25 +2,26 @@ import { useEffect, useState } from "react";
 import Card from "./Card";
 import Container from "../Shared/Container";
 import { useSearchParams } from "react-router-dom";
+import Heading from "../Shared/Heading";
 
 const Rooms = () => {
     const [rooms, setRooms] = useState([]);
 
-    const [params, setParams] = useSearchParams()
-    const category = params?.get('category')
+    const [params, setParams] = useSearchParams();
+    const category = params?.get("category");
     // console.log(category);
-
 
     useEffect(() => {
         fetch("rooms.json")
             .then((res) => res.json())
             .then((data) => {
-                if(category) {
-                    const filtered = data.filter( room => room?.category === category)
-                    setRooms(filtered)
-                }
-                else{
-                    setRooms(data)
+                if (category) {
+                    const filtered = data.filter(
+                        (room) => room?.category === category
+                    );
+                    setRooms(filtered);
+                } else {
+                    setRooms(data);
                 }
             });
     }, [category]);
@@ -28,11 +29,15 @@ const Rooms = () => {
 
     return (
         <Container>
-            <div className=" pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-                {rooms?.map((room) => (
-                    <Card key={room?._id} room={room} />
-                ))}
-            </div>
+            {rooms && rooms?.length > 0 ? (
+                <div className=" pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+                    {rooms?.map((room) => (
+                        <Card key={room?._id} room={room} />
+                    ))}
+                </div>
+            ) : (
+                <Heading></Heading>
+            )}
         </Container>
     );
 };
